b0fc4642272c3436e205174a30bcb8c40dfdd6f1
webmav Extensa Ubuntu <294071804+Webmav@users.noreply.github.com>
2026-09-15 20:21:49 +0530

    *Made biryani page semi-responsive *Made a script.js

---
+++ recipes/script.js
@@ -0,0 +1,25 @@
+// biryani
+const button = document.querySelectorAll("button");
+const iList = document.querySelector(".i-list");
+const sStep = document.querySelector(".s-steps");
+
+button.forEach((butt) => {
+    butt.addEventListener('click', ()=>{
+        if (butt.id=="iB") {
+            if(iList.style.display === ""){
+                iList.style.display = "block";
+            }
+            else if(iList.style.display == "block"){
+                iList.style.display = "";
+            }
+        }
+        else if (butt.id == "sB"){
+            if(sStep.style.display == ""){
+                sStep.style.display = "block";
+            }
+            else if(sStep.style.display == "block"){
+                sStep.style.display = "";
+            }
+        }
+    })
+})
