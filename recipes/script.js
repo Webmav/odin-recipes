62168a527dc545cf5bc768273935a40bd98cfb6c
WebMav <294071804+Webmav@users.noreply.github.com>
2026-09-20 17:46:44 +0530

    Made burger icon active

--- recipes/script.js
+++ recipes/script.js
@@ -1,31 +1,7 @@
-const navImg = document.querySelector("#navImg");
+const navImg = document.getElementById("navImgButton");
+const pB = document.querySelector(".pB");
 
-navImg.addEventListener('click', function() {
-    console.log("heee");
-})
-
-// biryani
-const button = document.querySelectorAll("button");
-const iList = document.querySelector(".i-list");
-const sStep = document.querySelector(".s-steps");
-
-button.forEach((butt) => {
-    butt.addEventListener('click', ()=>{
-        if (butt.id=="iB") {
-            if(iList.style.display === ""){
-                iList.style.display = "block";
-            }
-            else if(iList.style.display == "block"){
-                iList.style.display = "";
-            }
-        }
-        else if (butt.id == "sB"){
-            if(sStep.style.display == ""){
-                sStep.style.display = "block";
-            }
-            else if(sStep.style.display == "block"){
-                sStep.style.display = "";
-            }
-        }
-    })
+navImg.addEventListener('click', ()=>{
+    console.log("click");
+    pB.classList.toggle('open');
 })
